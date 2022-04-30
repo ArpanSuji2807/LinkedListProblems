35a12795d7e7e6404c594d380aea4c63b8f4f5e7
Praan26 <arpanap102@gmail.com>
2022-04-30 11:05:45 +0530

    [Arpan] Add: Simple linked list added

---
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace LinkedListProblem;
+class program
+{
+    public static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+        bool check=true;
+        Console.WriteLine("1.Simple Linked List");
+        while(check)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    list.Add(30);
+                    list.Add(56);
+                    list.Add(70);
+                    break;
+            }
+        }
+    }
+}
